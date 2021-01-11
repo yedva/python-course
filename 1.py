b55d63810d505939b147028b4599f2086377d0fa
ruslan <a@ru.ru>
2021-01-11 18:38:15 +0300

    Сделано

--- 1.py
+++ 1.py
@@ -1,11 +1,31 @@
 """
-Создать список и заполнить его элементами различных типов данных.
-Реализовать скрипт проверки типа данных каждого элемента.
-Использовать функцию type() для проверки типа.
-Элементы списка можно не запрашивать у пользователя, а указать явно, в программе.
+Реализовать функцию, принимающую два числа (позиционные аргументы)
+и выполняющую их деление. Числа запрашивать у пользователя,
+предусмотреть обработку ситуации деления на ноль.
 """
 
-multi_type_list = [10, .5, 'hello', None, False, (1, 2), ['a', 'b'], {'key': 'value'}, {'value_1', 'value_2'}]
 
-for el in multi_type_list:
-    print(type(el), el)
+def divide(a, b):
+    result = 0
+    if a > 0 and b > 0:
+        result = a / b
+    return result
+
+
+def ask_number(text):
+    user_input = input(text)
+    if user_input.isnumeric():
+        return int(user_input)
+    else:
+        return ask_number(f'{user_input} не число, попробуйте еще раз: ')
+
+
+def main():
+    a = ask_number('Введите делимое: ')
+    b = ask_number('Введите делитель: ')
+
+    print(f'Частное: {divide(a, b)}\n')
+    main()
+
+
+main()
--- 2.py
+++ 2.py
@@ -1,17 +1,28 @@
 """
-Для списка реализовать обмен значений соседних элементов.
-Значениями обмениваются элементы с индексами 0 и 1, 2 и 3 и т. д.
-При нечётном количестве элементов последний сохранить на своём месте.
-Для заполнения списка элементов нужно использовать функцию input().
+Реализовать функцию, принимающую несколько параметров,
+описывающих данные пользователя: имя, фамилия, год рождения,
+город проживания, email, телефон. Функция должна принимать
+параметры как именованные аргументы. Реализовать вывод данных
+о пользователе одной строкой.
 """
 
-some_list = []
-some_list.extend(input('скажи любое слово, я перемешаю соседние буковки: '))
 
-for i in range(len(some_list) // 2):
-    a_ind = i * 2
-    b_ind = a_ind + 1
-    a_val, b_val = some_list[a_ind], some_list[b_ind]
-    some_list[a_ind] = b_val
-    some_list[b_ind] = a_val
-print(''.join(some_list))
+def print_form(
+        first_name='',
+        last_name='',
+        birth_date='',
+        birthplace='',
+        mail='',
+        phone=''
+):
+    print(f'\n{first_name} {last_name}, род. {birth_date} в {birthplace}, почта {mail}, номер {phone}')
+
+
+print_form(
+    first_name=input('Ваше имя: '),
+    last_name=input('Фамилия: '),
+    birth_date=input('Дата рождения: '),
+    birthplace=input('Место рождения: '),
+    mail=input('Почта: '),
+    phone=input('Телефон: '),
+)
