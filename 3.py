8b626b19cbd6c3b21146cdf031f825a066780778
ruslan <rus@rus.rus>
2021-01-19 19:35:13 +0300

    Сделано

--- 3.py
+++ 3.py
@@ -1,8 +1,22 @@
 """
-Для чисел в пределах от 20 до 240 найти числа, кратные 20 или 21.
-Необходимо решить задание в одну строку.
-
-Подсказка: использовать функцию range() и генератор.
+Создать текстовый файл (не программно), построчно записать фамилии сотрудников и величину их окладов.
+Определить, кто из сотрудников имеет оклад менее 20 тыс., вывести фамилии этих сотрудников.
+Выполнить подсчет средней величины дохода сотрудников.
 """
 
-print([num for num in range(20, 241) if num % 20 == 0 or num % 21 == 0])
+
+def format_line(line):
+    return {
+        'last_name': line[0],
+        'wage': int(line[1])
+    }
+
+
+employees = list(map(format_line, [line.strip().split(',') for line in open('3.txt', 'r')]))
+
+print('Сотрудники с зп меньше 20000:')
+for employee_with_wage_lt_20k in filter(lambda e: e['wage'] < 20000, employees):
+    print(employee_with_wage_lt_20k['last_name'])
+
+wage_sum = sum(map(lambda e: e['wage'], employees))
+print(f'Средний доход всех сотрудников {wage_sum / len(employees)}')
